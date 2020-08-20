ac2ff39b6d73b40c0d939e07373273311f894683
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-08-20 09:34:34 +0300

    edit link pattern

--- models/card.js
+++ models/card.js
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     required: [true, 'это поле является обязательным для заполения'],
     validate: {
       validator(v) {
-        return /(^https?:\/\/)?[a-z0-9~_\-\.]{2,}\.[a-z]{1,9}[aA-zZ0-9~_\-\./?=&]+/.test(v);
+        return /(^https?:\/\/)?[a-z0-9~_\-.]{2,}\.[a-z]{1,9}[aA-zZ0-9~_\-./?=&]+/.test(v);
       },
       message: 'передана некорректная ссылка',
     },
